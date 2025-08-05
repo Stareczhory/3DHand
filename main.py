a6249dfa8379343aad5f625ddc7f0a2da90ab5b1
jakub lach <jakub.lach@gmail.com>
2025-08-05 17:54:02 +0200

    needs fixes

--- main.py
+++ main.py
@@ -1,29 +1,79 @@
+import csv
+from operator import index
+import time
+
 from vpython import *
+import pandas as pd
 
 scene.title = "3D Scene from PyCharm"
 
-wrist = (0,0,0)
-index_mcp = (-1.5,2,0)
-middle_mcp = (-0.5,2,0)
-ring_mcp = (0.5,2,0)
-pinky_mcp = (1.5, 2, 0)
-index_tip = (-1.5,4,0)
-middle_tip = (-0.5,4,0)
-ring_tip = (0.5,4,0)
-pinky_tip = (1.5, 4, 0)
+path = r'C:\Users\jakub\PycharmProjects\Data Processing\tracking_data.csv'
 
-points(pos=[wrist, index_mcp, middle_mcp, ring_mcp, pinky_mcp, index_tip,middle_tip,ring_tip,pinky_tip])
+rows = []
 
-wrist_index_mcp_line = cylinder(pos=vector(*wrist), axis=vector(*index_mcp)-vector(*wrist), radius=0.05, color=color.cyan)
-wrist_middle_mcp_line = cylinder(pos=vector(*wrist), axis=vector(*middle_mcp)-vector(*wrist), radius=0.05, color=color.cyan)
-wrist_ring_mcp_line = cylinder(pos=vector(*wrist), axis=vector(*ring_mcp)-vector(*wrist), radius=0.05, color=color.cyan)
-wrist_pinky_mcp_line = cylinder(pos=vector(*wrist), axis=vector(*pinky_mcp)-vector(*wrist), radius=0.05, color=color.cyan)
+with open(path, newline='', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if len(row) >= 8:
+            rows.append(row[:8:2])  # Take only every second value
+        # Else: skip the row or log a warning
 
-index_mcp_index_tip_line = cylinder(pos=vector(*index_mcp), axis=vector(*index_tip)-vector(*index_mcp), radius=0.05, color=color.cyan)
-middle_mcp_middle_tip_line = cylinder(pos=vector(*middle_mcp), axis=vector(*middle_tip)-vector(*middle_mcp), radius=0.05, color=color.cyan)
-ring_mcp_ring_tip_line = cylinder(pos=vector(*ring_mcp), axis=vector(*ring_tip)-vector(*ring_mcp), radius=0.05, color=color.cyan)
-pinky_mcp_pinky_tip_line = cylinder(pos=vector(*pinky_mcp), axis=vector(*pinky_tip)-vector(*pinky_mcp), radius=0.05, color=color.cyan)
 
-# Keep the scene alive
-while True:
-    rate(60)  # 60 frames per second
+data = pd.DataFrame(rows).astype('int')
+
+wrist = vector(0,0,0)
+index_mcp = vector(-1.5,4,0)
+middle_mcp = vector(-0.5,4,0)
+ring_mcp = vector(0.5,4,0)
+pinky_mcp = vector(1.5, 4, 0)
+index_tip = vector(-1.5,7,0)
+middle_tip = vector(-0.5,7,0)
+ring_tip = vector(0.5,7,0)
+pinky_tip = vector(1.5, 7, 0)
+
+# index_pip = vector(-1.5,5,0)
+# middle_pip = vector(-0.5,5,0)
+# ring_pip = vector(0.5,5,0)
+# pinky_pip = vector(1.5, 5, 0)
+#
+# index_dip = vector(-1.5,6,0)
+# middle_dip = vector(-0.5,6,0)
+# ring_dip = vector(0.5,6,0)
+# pinky_dip = vector(1.5, 6, 0)
+
+tip_vectors = [index_tip, middle_tip, ring_tip, pinky_tip]
+
+hand_points = points(pos=[wrist, index_mcp, middle_mcp, ring_mcp, pinky_mcp])
+
+wrist_index_mcp_line = cylinder(pos=wrist, axis=index_mcp - wrist, radius=0.05, color=color.cyan)
+wrist_middle_mcp_line = cylinder(pos=wrist, axis=middle_mcp - wrist, radius=0.05, color=color.cyan)
+wrist_ring_mcp_line = cylinder(pos=wrist, axis=ring_mcp - wrist, radius=0.05, color=color.cyan)
+wrist_pinky_mcp_line = cylinder(pos=wrist, axis=pinky_mcp - wrist, radius=0.05, color=color.cyan)
+
+index_mcp_index_tip_line = cylinder(pos=index_mcp, axis=index_tip - index_mcp, radius=0.05, color=color.cyan)
+middle_mcp_middle_tip_line = cylinder(pos=middle_mcp, axis=middle_tip - middle_mcp, radius=0.05, color=color.cyan)
+ring_mcp_ring_tip_line = cylinder(pos=ring_mcp, axis=ring_tip - ring_mcp, radius=0.05, color=color.cyan)
+pinky_mcp_pinky_tip_line = cylinder(pos=pinky_mcp, axis=pinky_tip - pinky_mcp, radius=0.05, color=color.cyan)
+
+for i, row in data.iterrows():
+    rate(20)
+    angles = row
+    for idx, angle in enumerate(angles):
+
+        tip_coords = tip_vectors[idx]
+
+        length_a = 4
+        length_b = 3
+        length_c = sqrt(length_a**2 + length_b**2 - 2*length_a*length_b*cos(angle))
+        z_coordinate = sqrt(length_b**2 - ((length_c**2 - length_b**2 + length_a**2)/(2*length_a)-length_a)**2)
+        y_coordinate = (length_c**2 - length_b**2 + length_a**2)/(2*length_a)
+
+        tip_coords.y = y_coordinate
+        tip_coords.z = z_coordinate
+
+    index_mcp_index_tip_line.axis = index_tip - index_mcp
+    middle_mcp_middle_tip_line.axis = middle_tip - middle_mcp
+    ring_mcp_ring_tip_line.axis = ring_tip - ring_mcp
+    pinky_mcp_pinky_tip_line.axis = pinky_tip - pinky_mcp
+
+
---
+++ more_complex_hand.py
@@ -0,0 +1,121 @@
+import csv
+from operator import index
+import time
+
+from vpython import *
+import pandas as pd
+
+scene.title = "3D Scene from PyCharm"
+
+path = r'C:\Users\jakub\PycharmProjects\Data Processing\tracking_data.csv'
+
+rows = []
+
+with open(path, newline='', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if len(row) >= 8:
+            rows.append(row[:8:2])  # Take only every second value
+        # Else: skip the row or log a warning
+
+
+data = pd.DataFrame(rows).astype('int')
+
+cylinder_radius = 0.10
+
+wrist = vector(0,0,0)
+index_mcp = vector(-1.5,4,0)
+middle_mcp = vector(-0.5,4,0)
+ring_mcp = vector(0.5,4,0)
+pinky_mcp = vector(1.5, 4, 0)
+index_tip = vector(-1.5,7,0)
+middle_tip = vector(-0.5,7,0)
+ring_tip = vector(0.5,7,0)
+pinky_tip = vector(1.5, 7, 0)
+
+index_pip = vector(-1.5,5,0)
+middle_pip = vector(-0.5,5,0)
+ring_pip = vector(0.5,5,0)
+pinky_pip = vector(1.5, 5, 0)
+
+index_dip = vector(-1.5,6,0)
+middle_dip = vector(-0.5,6,0)
+ring_dip = vector(0.5,6,0)
+pinky_dip = vector(1.5, 6, 0)
+
+tip_vectors = [index_tip, middle_tip, ring_tip, pinky_tip]
+dip_vectors = [index_dip, middle_dip, ring_dip, pinky_dip]
+pip_vectors = [index_pip, middle_pip, ring_pip, pinky_pip]
+mcp_vectors = [index_mcp, middle_mcp, ring_mcp, pinky_mcp]
+
+hand_points = points(pos=[wrist, index_mcp, middle_mcp, ring_mcp, pinky_mcp])
+
+wrist_index_mcp_line = cylinder(pos=wrist, axis=index_mcp - wrist, radius=cylinder_radius, color=color.cyan)
+wrist_middle_mcp_line = cylinder(pos=wrist, axis=middle_mcp - wrist, radius=cylinder_radius, color=color.cyan)
+wrist_ring_mcp_line = cylinder(pos=wrist, axis=ring_mcp - wrist, radius=cylinder_radius, color=color.cyan)
+wrist_pinky_mcp_line = cylinder(pos=wrist, axis=pinky_mcp - wrist, radius=cylinder_radius, color=color.cyan)
+
+index_mcp_index_pip_line = cylinder(pos=index_mcp, axis=index_pip - index_mcp, radius=cylinder_radius, color=color.cyan)
+middle_mcp_middle_pip_line = cylinder(pos=middle_mcp, axis=middle_pip - middle_mcp, radius=cylinder_radius, color=color.cyan)
+ring_mcp_ring_pip_line = cylinder(pos=ring_mcp, axis=ring_pip - ring_mcp, radius=cylinder_radius, color=color.cyan)
+pinky_mcp_pinky_pip_line = cylinder(pos=pinky_mcp, axis=pinky_pip - pinky_mcp, radius=cylinder_radius, color=color.cyan)
+
+index_pip_dip_line = cylinder(pos=index_pip, axis=index_dip - index_pip, radius=cylinder_radius, color=color.cyan)
+middle_pip_dip_line = cylinder(pos=middle_pip, axis=middle_dip - middle_pip, radius=cylinder_radius, color=color.cyan)
+ring_pip_dip_line = cylinder(pos=ring_pip, axis=ring_dip - ring_pip, radius=cylinder_radius, color=color.cyan)
+pinky_pip_dip_line = cylinder(pos=pinky_pip, axis=pinky_dip - pinky_pip, radius=cylinder_radius, color=color.cyan)
+
+index_dip_tip_line = cylinder(pos=index_dip, axis=index_tip - index_dip, radius=cylinder_radius, color=color.cyan)
+middle_dip_tip_line = cylinder(pos=middle_dip, axis=middle_tip - middle_dip, radius=cylinder_radius, color=color.cyan)
+ring_dip_tip_line = cylinder(pos=ring_dip, axis=ring_tip - ring_dip, radius=cylinder_radius, color=color.cyan)
+pinky_dip_tip_line = cylinder(pos=pinky_dip, axis=pinky_tip - pinky_dip, radius=cylinder_radius, color=color.cyan)
+
+for i, row in data.iterrows():
+    rate(20)
+    angles = row.values
+    for idx, angle in enumerate(angles):
+        temp_angle = angle
+
+        tip_coords = tip_vectors[idx]
+        dip_coords = dip_vectors[idx]
+        pip_coords = pip_vectors[idx]
+        mcp_coords = mcp_vectors[idx]
+
+        finger_coords = [tip_coords, pip_coords, dip_coords]
+        for coords in finger_coords:
+            if coords == pip_coords:
+                length_a = 4
+                length_b = 1
+                temp_angle = 90 + (int(angle)/2)
+            elif coords == dip_coords:
+                length_a = 1
+                length_b = 1
+                temp_angle = 90 + (int(angle) / 2)
+            elif coords == tip_coords:
+                length_a = 1
+                length_b = 1
+                temp_angle = 150 + (int(angle) / 6)
+            else:
+                print('error')
+
+            length_c = sqrt(length_a**2 + length_b**2 - 2*length_a*length_b*cos(radians(temp_angle)))
+            z_coordinate = sqrt(length_b**2 - ((length_c**2 - length_b**2 + length_a**2)/(2*length_a)-length_a)**2)
+            y_coordinate = (length_c**2 - length_b**2 + length_a**2)/(2*length_a)
+
+            coords.y = y_coordinate
+            coords.z = z_coordinate
+
+    index_mcp_index_pip_line.axis = index_pip - index_mcp
+    middle_mcp_middle_pip_line.axis = middle_pip - middle_mcp
+    ring_mcp_ring_pip_line.axis = ring_pip - ring_mcp
+    pinky_mcp_pinky_pip_line.axis = pinky_pip - pinky_mcp
+
+    index_pip_dip_line.axis = index_dip - index_pip
+    middle_pip_dip_line.axis = middle_dip - middle_pip
+    ring_pip_dip_line.axis = ring_dip - ring_pip
+    pinky_pip_dip_line.axis = pinky_dip - pinky_pip
+
+    index_dip_tip_line.axis = index_tip - index_dip
+    middle_dip_tip_line.axis = middle_tip - middle_dip
+    ring_dip_tip_line.axis = ring_tip - ring_dip
+    pinky_dip_tip_line.axis = pinky_tip - pinky_dip
